7df572a7616d29729c8a35ec960e7dc8290e9ce1
Daniel Morris <daniel@honestempire.com>
2024-04-19 15:38:45 +0100

    fix: Fix name of the claude-3-haiku-20240307 const

--- model.go
+++ model.go
@@ -8,7 +8,7 @@ type LanguageModel string
 const (
 	Claude3Opus20240229   LanguageModel = "claude-3-opus-20240229"
 	Claude3Sonnet20240229 LanguageModel = "claude-3-sonnet-20240229"
-	Claude3Haiku20240229  LanguageModel = "claude-3-haiku-20240307"
+	Claude3Haiku20240307  LanguageModel = "claude-3-haiku-20240307"
 	Claude21              LanguageModel = "claude-2.1"
 	Claude20              LanguageModel = "claude-2.0"
 	ClaudeInstant12       LanguageModel = "claude-instant-1.2"
