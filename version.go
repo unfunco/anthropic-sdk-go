0d5f3cce46b5af30ab9361de404c147aacf481b4
David Letterman <48985810+david-letterman@users.noreply.github.com>
2024-05-14 08:31:36 +0000

    chore: Configure the version number

--- version.go
+++ version.go
@@ -2,4 +2,4 @@ package anthropic
 
 // semanticVersion does not need to be updated manually.
 // It is automatically updated by the release process.
-const semanticVersion = "0.1.0"
+const semanticVersion = "0.2.0"
