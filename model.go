d0f5ca9e9a964ba3ba8ca7e62adea048b9cef14c
Daniel Morris <daniel@honestempire.com>
2024-07-03 10:51:03 +0100

    feat: Add support for the Claude 3.5 Sonnet model (#27)

--- model.go
+++ model.go
@@ -6,10 +6,11 @@ package anthropic
 type LanguageModel string
 
 const (
-	Claude3Opus20240229   LanguageModel = "claude-3-opus-20240229"
-	Claude3Sonnet20240229 LanguageModel = "claude-3-sonnet-20240229"
-	Claude3Haiku20240307  LanguageModel = "claude-3-haiku-20240307"
-	Claude21              LanguageModel = "claude-2.1"
-	Claude20              LanguageModel = "claude-2.0"
-	ClaudeInstant12       LanguageModel = "claude-instant-1.2"
+	Claude35Sonnet20240620 LanguageModel = "claude-3-5-sonnet-20240620"
+	Claude3Opus20240229    LanguageModel = "claude-3-opus-20240229"
+	Claude3Sonnet20240229  LanguageModel = "claude-3-sonnet-20240229"
+	Claude3Haiku20240307   LanguageModel = "claude-3-haiku-20240307"
+	Claude21               LanguageModel = "claude-2.1"
+	Claude20               LanguageModel = "claude-2.0"
+	ClaudeInstant12        LanguageModel = "claude-instant-1.2"
 )
